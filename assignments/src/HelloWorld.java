89a0e81ba310d25848a013c26c8a2574b924dd19
Gerry Blais <gerry.blais@gmail.com>
2011-09-04 16:45:06 -0400

    update test

--- assignments/src/HelloWorld.java
+++ assignments/src/HelloWorld.java
@@ -1,18 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author gblais1
- */
 public class HelloWorld {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-System.out.println("Hello world!");
+System.out.println("Vaya con dios, amigo.");
         }
 }
