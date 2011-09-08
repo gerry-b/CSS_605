f44d35b48d0c89b87ade1bccb014e2630444b4ee
Gerry Blais <gerry.blais@gmail.com>
2011-09-08 14:43:23 -0400

    TEST:  This is the real Hello World

---
+++ hw1/HelloCss605.java
@@ -0,0 +1,14 @@
+class HelloCss605 {
+	public static void main(String[] args)
+	{
+	System.out.println("\nHello CSS 605!!!!\n") ;
+	
+	System.out.println("\nNothing has made me like Python more than relearning Java this past week.\n") ;
+	
+	
+	
+	
+	}
+
+
+}
